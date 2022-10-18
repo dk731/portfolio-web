3ef9bce7e6d75f5442f21ad0bb693e14571d3097
Demid Kaidalov <demid.kaidalov@gmail.com>
2022-10-19 00:01:33 +0300

    Added corrent selection and focus overlays for desktop icon

--- src/components/win95/base/Win95DesktopIcon.vue
+++ src/components/win95/base/Win95DesktopIcon.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { onMounted, onUnmounted } from "vue";
+import { onMounted, onUnmounted, ref } from "vue";
 
 import { useDesktopState } from "@/stores/Win95DesktopState";
 
@@ -12,28 +12,34 @@ const props = defineProps<{
   onOpenClb: () => void;
 }>();
 
+const isSelected = ref<boolean>(true);
+
 const desktopState = useDesktopState();
 const myId = uuid4();
 
 var isDoubleClick = false;
 function onMouseClick(e: MouseEvent) {
   desktopState.desktop.focusedApp = myId;
+  if (isDoubleClick) {
+    isDoubleClick = false;
+    runOpenClb();
+  }
+
+  isDoubleClick = true;
+  setTimeout(() => (isDoubleClick = false), 300);
 }
 
 const MIN_OPEN_DELAY = 500;
 var lastOpenTime: number = 0;
 function runOpenClb() {
-  if (
-    desktopState.desktop.focusedApp == myId &&
-    moment().valueOf() - lastOpenTime > MIN_OPEN_DELAY
-  ) {
+  if (moment().valueOf() - lastOpenTime > MIN_OPEN_DELAY) {
     lastOpenTime = moment().valueOf();
     props.onOpenClb();
   }
 }
 
 function onKeyPress(e: KeyboardEvent) {
-  if (e.key == "Enter") runOpenClb();
+  if (e.key == "Enter" && desktopState.desktop.focusedApp == myId) runOpenClb();
 }
 
 onMounted(() => document.addEventListener("keypress", onKeyPress));
@@ -45,13 +51,22 @@ onUnmounted(() => document.removeEventListener("keypress", onKeyPress));
     <div
       class="desktop-icon-image"
       :style="{ backgroundImage: `url(${props.icon})` }"
-    />
-    <div
-      :class="`desktop-icon-title ${
-        desktopState.desktop.focusedApp == myId ? 'focused' : ''
-      }`"
     >
+      <div
+        v-if="isSelected"
+        :style="{
+          WebkitMaskImage: `url(${props.icon})`,
+          maskImage: `url(${props.icon})`,
+        }"
+        class="desktop-icon-overlay"
+      ></div>
+    </div>
+    <div :class="`desktop-icon-title ${isSelected ? 'selected' : ''}`">
       {{ props.title }}
+      <div
+        v-if="desktopState.desktop.focusedApp == myId"
+        class="desktop-title-border"
+      ></div>
     </div>
   </div>
 </template>
@@ -64,25 +79,50 @@ onUnmounted(() => document.removeEventListener("keypress", onKeyPress));
   flex-direction: column;
   align-items: center;
 
+  box-sizing: border-box;
+
   top: 100px;
   left: 100px;
 }
 
 .desktop-icon-image {
+  position: relative;
+
   width: 32px;
   height: 32px;
 
-  margin-bottom: 4px;
+  margin-bottom: 3px;
 
   background-size: 100%;
 }
 
+.desktop-icon-overlay {
+  position: absolute;
+
+  left: 0px;
+  top: 0px;
+
+  width: 100%;
+  height: 100%;
+
+  background: repeating-conic-gradient(#0000a8 0% 25%, transparent 0% 50%) 50% /
+    2px 2px;
+
+  -webkit-mask-size: 100%;
+  mask-size: 100%;
+  -webkit-mask-repeat: no-repeat;
+  mask-repeat: no-repeat;
+  -webkit-mask-position: center;
+  mask-position: center;
+}
+
 .desktop-icon-title {
+  position: relative;
   left: 0;
 
   max-width: 68px;
 
-  padding: 1px;
+  padding: 2px;
 
   box-sizing: border-box;
 
@@ -93,7 +133,23 @@ onUnmounted(() => document.removeEventListener("keypress", onKeyPress));
   color: #ffffff;
 }
 
-.desktop-icon-title.focused {
-  border: 1px dotted #ff0000;
+.desktop-icon-title.selected {
+  background-color: #0000a8;
+}
+
+.desktop-title-border {
+  position: absolute;
+
+  left: 0px;
+  top: 0px;
+
+  width: 100%;
+  height: 100%;
+
+  border: 1px dotted #ff9191;
+  box-sizing: border-box;
+  mix-blend-mode: difference;
+
+  padding: 1px;
 }
 </style>

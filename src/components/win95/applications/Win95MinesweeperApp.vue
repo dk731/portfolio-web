0b96d007861040652f4c54678c35eeb43db0ab91
Demid Kaidalov <demid.kaidalov@gmail.com>
2022-10-27 01:15:13 +0300

    Added explorer and minesweeper application templates

---
+++ src/components/win95/applications/Win95MinesweeperApp.vue
@@ -0,0 +1,20 @@
+<script lang="ts" setup>
+import Win95Application from "../base/Win95DesktopApplication.vue";
+</script>
+
+<template>
+  <Win95Application
+    :id="`minesweeper-app`"
+    :icon="`images/win95/minesweeper-mine.png`"
+    :title="`Minesweeper`"
+    :init-icon="{ position: { x: 10, y: 200 } }"
+    :init-window="{
+      position: { x: 50, y: 50 },
+      size: { width: 300, height: 150 },
+    }"
+  >
+    <template #content>123</template>
+  </Win95Application>
+</template>
+
+<style scoped></style>
---
+++ src/components/win95/applications/Win95TheInternetApp.vue
@@ -0,0 +1,20 @@
+<script lang="ts" setup>
+import Win95Application from "../base/Win95DesktopApplication.vue";
+</script>
+
+<template>
+  <Win95Application
+    :id="`the-internet-app`"
+    :icon="`images/win95/the-internet.png`"
+    :title="`The Internet`"
+    :init-icon="{ position: { x: 10, y: 140 } }"
+    :init-window="{
+      position: { x: 50, y: 50 },
+      size: { width: 300, height: 150 },
+    }"
+  >
+    <template #content>123</template>
+  </Win95Application>
+</template>
+
+<style scoped></style>

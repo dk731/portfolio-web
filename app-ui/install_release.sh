251aa7cbc53d3dab4e11331b50959fee12cf6c65
Demid Kaidalov <demid.kaidalov@gmail.com>
2022-11-26 15:12:29 +0200

    Fixed github flow

--- app-ui/install_release.sh
+++ app-ui/install_release.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
 echo "Unpacking latest portfolio artifact"
-unzip -o ~/portfolio_artifact.zip -d ~/portfolio_release
-
-# echo "Unpacking latest blog artifact"
-# unzip -o ~/blog_artifact.zip -d ~/blog_release
+unzip -o ~/app-ui.zip -d ~/portfolio_release
 
